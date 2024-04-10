11c8ac0a2677a0795fe059877b67d844618bb7ce
Jack Skehan <jskehan@c1b2c3.42berlin.de>
2024-04-10 21:24:01 +0200

    wahh

--- src/pipe.c
+++ src/pipe.c
@@ -6,7 +6,7 @@
 /*   By: Jskehan <jskehan@student.42Berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:22:57 by Jskehan           #+#    #+#             */
-/*   Updated: 2024/04/10 17:35:19 by Jskehan          ###   ########.fr       */
+/*   Updated: 2024/04/10 21:21:07 by Jskehan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,15 @@ void	execute_child_process(int *p_fd, char *cmd, char **envp)
 	exit(ret);
 }
 
-void	execute_parent_process(int *p_fd, pid_t pid)
+void	execute_parent_process(int *p_fd, pid_t pid, char *cmd)
 {
 	int	status;
 
 	close(p_fd[1]);
 	dup2(p_fd[0], 0);
-	waitpid(pid, &status, 0);
+	perror(cmd);
+	if (ft_strncmp(cmd, "sleep", 5) == 0)
+		waitpid(pid, &status, 0);
 }
 
 void	pipe_process(char *cmd, char **envp)
@@ -63,5 +65,5 @@ void	pipe_process(char *cmd, char **envp)
 	if (!pid)
 		execute_child_process(p_fd, cmd, envp);
 	else
-		execute_parent_process(p_fd, pid);
+		execute_parent_process(p_fd, pid, cmd);
 }
